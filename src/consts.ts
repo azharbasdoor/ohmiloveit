a4a86beb8f422911431ccea87fdc76fe384b25d1
azharbasdoor <azharbasdoor@outlook.com>
2026-02-02 10:58:27 +0700

    Change site title and description
    Updated site title and description to reflect new branding.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Masriah Hasan";
+export const SITE_DESCRIPTION = "Jelajahi rasa, destinasi, dan kecantikan bersama Masriah Hasan! Selamat datang di dunia Masriah Hasan! Temukan petualangan kuliner, perjalanan seru, dan tips kecantikan khusus untuk kulit kering. Sambut keindahan hidup dari perspektif unik dalam setiap cerita yang dibagikan.";
